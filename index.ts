d0bcb8e89757ca8c05cf00b48742ffd29ea80780
Nikita Shchypylov <shchypylov@gmail.com>
2019-07-14 23:03:42 +0300

    small backend adjustments

--- index.ts
+++ index.ts
@@ -1,18 +1,25 @@
 const express = require("express");
 const port = process.env.port || 3000;
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
+
 const strifes = [
     {
+        id: 1,
         title: 'First strife',
         author: 'Some guy',
         tags: ['Dummy', 'Fun']
     },
     {
+        id: 2,
         title: 'Second strife',
         author: 'Some guy',
         tags: ['Dummy', 'Fun']
     },
     {
+        id: 3,
         title: 'Third strife',
         author: 'Some guy',
         tags: ['Dummy', 'Fun']
